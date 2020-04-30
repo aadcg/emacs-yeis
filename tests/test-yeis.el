fca05eb5394d97e224de35d876c09f1d078a9bda
André Alexandre Gomes <andremegafone@gmail.com>
2020-04-30 22:32:06 +0100

    Added skeleton for future work

---
+++ tests/test-yeis.el
@@ -0,0 +1,21 @@
+;;; yeis.el --- Yeis's Emacs' Input Switcher
+
+;; Copyright © 2020 André A. Gomes <andremegafone@gmail.com>
+
+;; Version: 0.1
+;; URL: https://github.com/aadcg/emacs-yeis
+
+;; Yeis is NOT part of GNU Emacs.
+
+;; Yeis is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free
+;; Software Foundation; either version 3, or (at your option) any later
+;; version.
+
+;; Yeis is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+;; for more details.
+
+;; For a full copy of the GNU General Public License see
+;; <http://www.gnu.org/licenses/>.
